0fb359a870f0a23df2b1e1e6d8ca8f2812b71ae1
iamumer11 <iamumerawan@gmail.com>
2020-09-13 16:24:41 +0500

    Python coding exercises

---
+++ GuessingChallenge.py
@@ -0,0 +1,9 @@
+food = input("What is my favourite food?")
+
+if food == "Alfredo":
+    print("Yep! That's So Amazing")
+
+else:
+    print("Yuck! that's not it")
+
+print("Thanks for playing") 
---
+++ favouriteCities.py
@@ -0,0 +1,7 @@
+def favourite_city(name):
+    print("One of my favourite cities is",name)
+
+favourite_city("MELBOURNE")
+favourite_city("ISLAMABAD")
+favourite_city("GUJRANWALA")
+
